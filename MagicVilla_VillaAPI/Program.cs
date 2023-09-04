eb854b806415feb179e211626a55e9c6fd44d4dc
Sunil Kumar Pashikanti <36616898+spashikanti@users.noreply.github.com>
2023-09-04 05:12:26 +0000

    Added EFCore (#4)
    Co-authored-by: Sunil Kumar Pashikanti <sunil.pashikanti@hotmail.com>

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,7 +1,15 @@
+using MagicVilla_VillaAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDbContext>(
+    //option =>
+    //{
+    //    option.UseMySQL(builder.Configuration.GetConnectionString("klnmtr"));
+    //}
+);
 builder.Services.AddControllers(
     option =>
     {
