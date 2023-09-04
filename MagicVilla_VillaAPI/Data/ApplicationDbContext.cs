e20247cfcc584e44cbdf4ab50618fca451758707
Sunil Kumar Pashikanti <sunil.pashikanti@hotmail.com>
2023-09-04 02:58:27 +0000

    Added EFCore

---
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,64 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            //var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            //optionsBuilder.UseMySQL("server=localhost;user id=root;password=Welcome@12345;database=klnmtr");
+            //var _context = new ApplicationDbContext(optionsBuilder.Options);
+        }
+
+        public ApplicationDbContext(string connectionString) : base(GetOptions(connectionString))
+        {
+        }
+
+        private static DbContextOptions GetOptions(string connectionString)
+        {
+            if(string.IsNullOrEmpty(connectionString))
+            {
+                connectionString = "klnmtr";
+            }
+            string path = "server=localhost;user id=root;password=Welcome@12345;database=" + connectionString;
+            return MySQLDbContextOptionsExtensions.UseMySQL(new DbContextOptionsBuilder(), connectionString).Options;
+        }
+
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Villa>()
+                .HasData(
+                    new Villa()
+                    {
+                        Id = 1,
+                        Name = "Royal Villa",
+                        Details = "Royal Villa Details",
+                        ImageUrl = "https://v7n2u8v7.rocketcdn.me/wp-content/uploads/2018/09/229DeF2-L3-1.jpg",
+                        Occupancy = 5,
+                        Rate = 200,
+                        Sqft = 500,
+                        Amenity = "",
+                        CreatedDate = DateTime.Now
+                    }
+                );
+        }
+    }
+
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseMySQL("server=localhost;user id=root;password=Welcome@12345;database=klnmtr");
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Migrations/20230904025625_SeedVillaTable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_VillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedVillaTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenity", "CreatedDate", "Details", "ImageUrl", "Name", "Occupancy", "Rate", "Sqft", "updatedDate" },
+                values: new object[] { 1, "", new DateTime(2023, 9, 4, 2, 56, 25, 294, DateTimeKind.Local).AddTicks(1948), "Royal Villa Details", "https://v7n2u8v7.rocketcdn.me/wp-content/uploads/2018/09/229DeF2-L3-1.jpg", "Royal Villa", 5, 200.0, 500, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
